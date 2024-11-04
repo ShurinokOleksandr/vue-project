82c9788e298cee740f3c7e6413c971920bb95a10
shuru <shurunok0@gmail.com>
2024-11-04 19:38:57 +0200

    Make input with modal  and navigation

--- src/components/ui/input/Input.vue
+++ src/components/ui/input/Input.vue
@@ -18,8 +18,9 @@ const modelValue = useVModel(props, 'modelValue', emits, {
   defaultValue: props.defaultValue,
   passive: true,
 })
-</script> 
+</script>
+
 
 <template>
-    <input :class="cn('flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50', props.class)" v-model="modelValue">
+    <input :class="cn('flex outline-none rounded py-2 px-4  border bg-surface text-sm text-surfaceText disabled:cursor-not-allowed disabled:opacity-50', props.class)" v-model="modelValue">
 </template>
