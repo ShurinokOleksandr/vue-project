6abafe63ecba5ab7ba95c1fe2450b8aa11e51e61
shuru <shurunok0@gmail.com>
2025-01-24 18:01:47 +0200

    Change project structure

---
+++ src/shared/components/ui/avatar/Avatar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+
+import { AvatarRoot } from 'radix-vue'
+import { cn } from '@/lib/utils'
+
+import { type AvatarVariants, avatarVariant } from './index'
+
+const props = withDefaults(defineProps<{
+  class?: HTMLAttributes['class']
+  shape?: AvatarVariants['shape']
+  size?: AvatarVariants['size']
+}>(), {
+  shape: 'circle',
+  size: 'sm',
+})
+</script>
+
+<template>
+    <AvatarRoot :class="cn(avatarVariant({ size, shape }), props.class)">
+        <slot />
+    </AvatarRoot>
+</template>
---
+++ src/shared/components/ui/avatar/AvatarImage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { type AvatarImageProps, AvatarImage } from 'radix-vue'
+
+const props = defineProps<AvatarImageProps>()
+console.log(props)
+</script>
+
+<template>
+    <AvatarImage v-bind="props" class="h-full w-full object-cover" />
+</template>
+ 
